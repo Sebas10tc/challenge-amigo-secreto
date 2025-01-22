7e1147cd993b8370ae4da23964c9103e0189a456
sebas10tc <tenoriosebastian2004@gmail.com>
2025-01-22 00:18:07 -0500

    Implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos=[];
+
+function cambiarElemento(elemento, mensaje){
+    let elementoHTML=document.querySelector(elemento);
+    elementoHTML.innerHTML=mensaje;
+    return;
+}
+function agregarAmigos(amigos) {
+
+    cambiarElemento('#listaAmigos', '')
+     for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = amigos[i];
+        document.getElementById('listaAmigos').appendChild(li);
+    }
+}
+
+function limpiarCaja(){
+    document.querySelector("#amigo").value='';
+}
+
+
+function agregarAmigo(){
+    let amigo=document.querySelector("#amigo").value;
+
+    if(amigo==''){
+        alert("Por favor, inserte un nombre");
+    }
+    else{
+        amigos.push(amigo);
+        limpiarCaja();
+        agregarAmigos(amigos);
+        return
+    }
+}
+
