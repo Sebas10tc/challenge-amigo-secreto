260ca969adf821f5b4925340f6505f126981685d
sebas10tc <tenoriosebastian2004@gmail.com>
2025-01-23 15:44:58 -0500

    Agregar funcion sortear y limpiar

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function cambiarElemento(elemento, mensaje){
     elementoHTML.innerHTML=mensaje;
     return;
 }
-function agregarAmigos(amigos) {
+function agregarAmigosVisible(amigos) {
 
     cambiarElemento('#listaAmigos', '')
      for (let i = 0; i < amigos.length; i++) {
@@ -19,19 +19,55 @@ function agregarAmigos(amigos) {
 function limpiarCaja(){
     document.querySelector("#amigo").value='';
 }
+function limpiarlista(){
+    amigos=[];
+    limpiarCaja();
+    agregarAmigosVisible(amigo);
+}
 
+function colorearBoton(){
+    if(document.querySelector('#amigo').value!=''){
+        document.getElementById("agregar").style.backgroundColor="red"
+    } else {
+        document.getElementById("agregar").style.backgroundColor = ""
+    }
+    document.getElementById("amigo").addEventListener("input", colorearBoton);
+
+    
+}
 
 function agregarAmigo(){
     let amigo=document.querySelector("#amigo").value;
-
-    if(amigo==''){
-        alert("Por favor, inserte un nombre");
+    if(amigos.includes(amigo)){
+        alert("Nombre registrado en la lista")
+        return;
+    }else{
+        if(amigo==''){
+            alert("Por favor, inserte un nombre");
+        }
+        else{
+            amigos.push(amigo);
+            limpiarCaja();
+            colorearBoton();
+            agregarAmigosVisible(amigos);
+            return
     }
-    else{
-        amigos.push(amigo);
-        limpiarCaja();
-        agregarAmigos(amigos);
-        return
     }
+    
 }
 
+function sortearAmigo(){
+    if(amigos!=0){
+        let numerorAleatorio=Math.floor(Math.random()*amigos.length);
+        let amigoSecreto=amigos[numerorAleatorio];
+        cambiarElemento("#listaAmigos", ' ');
+        cambiarElemento("#resultado", `Tu amigo secreto es: ${amigoSecreto}`);
+    }
+    else{
+        alert("No hay elementos para sortear");
+        return
+    }
+
+}
+
+colorearBoton();
